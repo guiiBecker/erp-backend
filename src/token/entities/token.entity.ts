df6b7f453b7a0a21e70173c4ab6ba3e630d1818d
guiiBecker <guilhermegbecker@gmail.com>
2025-02-13 09:10:21 -0300

    feat: entity token and user

--- src/token/entities/token.entity.ts
+++ src/token/entities/token.entity.ts
@@ -1,1 +1,8 @@
-export class Token {}
+export class Token {
+    id?:string;
+    userId:number;
+    token:string;
+    createdAt: Date;
+    expiresdAt: Date;
+
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+import { Token } from "src/token/entities/token.entity";
+
+export class User {
+    id?:number;
+    name:string;
+    username:string;
+    password:string;
+    token:Token[];
+}
